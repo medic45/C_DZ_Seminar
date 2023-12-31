d3fd8e551023eecd9ace58a2cc6af2ccb2d189a6
Похламков Дмитрий Александрович <medic45rz@gmail.com>
2023-12-31 11:06:36 +0700

    Добавил все 4 задачи в Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+using System;
 
 class Program 
 {
@@ -21,3 +22,94 @@ class Program
 }
 
 
+//Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+using System;
+
+class Program 
+{
+  static void Main(string[] args) 
+  {
+    Console.Write("Введите координату X: ");
+    int x = int.Parse(Console.ReadLine());
+
+    Console.Write("Введите координату Y: ");  
+    int y = int.Parse(Console.ReadLine());
+
+    if(x > 0 && y > 0) 
+    {
+      Console.WriteLine("1");
+    }
+    else if(x < 0 && y > 0) 
+    {
+      Console.WriteLine("2");  
+    }
+    else if(x < 0 && y < 0) 
+    {
+      Console.WriteLine("3");
+    }
+    else if(x > 0 && y < 0) 
+    {
+      Console.WriteLine("4");
+    }
+  }
+}
+
+
+//Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+using System;
+
+class Program 
+{
+  static void Main(string[] args) 
+  {
+
+    Console.Write("Введите число от 10 до 99: ");
+    int num = int.Parse(Console.ReadLine());
+
+    if(num < 10 || num > 99) 
+    {
+      Console.WriteLine("Число не в диапазоне от 10 до 99");
+      return;
+    }
+
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+
+    int maxDigit = Math.Max(firstDigit, secondDigit);
+
+    Console.WriteLine("Наибольшая цифра числа: " + maxDigit);
+
+  }
+}
+
+
+//Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+using System;
+using System.Collections.Generic;
+
+class Program 
+{
+
+  static void Main(string[] args) 
+  {
+
+    Console.Write("Введите число: ");
+    int num = int.Parse(Console.ReadLine());
+
+    List<int> digits = new List<int>();
+
+    while (num > 0) 
+    {
+      digits.Insert(0, num % 10);  
+      num /= 10;
+    }
+
+    for(int i = 0; i < digits.Count; i++) 
+    {
+      if (i > 0) Console.Write(",");
+      Console.Write(digits[i]); 
+    }
+
+  }
+
+}
