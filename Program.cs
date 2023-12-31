544cf48747fc575321c1b05d4516466dbd74fa04
Похламков Дмитрий Александрович <medic45rz@gmail.com>
2023-12-31 10:58:11 +0700

    Добавил папку с ДЗ

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program 
+{
+  static void Main(string[] args) 
+  {
+    
+    Console.Write("а=");
+    int num = int.Parse(Console.ReadLine());
+
+    if(num % 7 == 0 && num % 23 == 0) 
+    {
+      Console.WriteLine("Да");
+    }
+    else 
+    {
+      Console.WriteLine("Нет"); 
+    }
+
+  }
+}
+
+
